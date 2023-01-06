df46b82c7f2e424c4f079e348b81092834903f73
Orianka13 <odobrogor@gmail.com>
2023-01-06 13:38:33 +0400

    Создан проект

---
+++ RickAndMortyApp/RickAndMortyApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Олеся Егорова on 06.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
