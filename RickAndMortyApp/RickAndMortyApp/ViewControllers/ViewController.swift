c9d58d87b5fe23d163e87567509de390a3d39736
Orianka13 <odobrogor@gmail.com>
2023-01-07 11:27:03 +0400

    Добавлен fetchRequest и вывод данных на консоль

--- RickAndMortyApp/RickAndMortyApp/ViewControllers/ViewController.swift
+++ RickAndMortyApp/RickAndMortyApp/ViewControllers/ViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchCharacter()
     }
-
-
+    
+    private func fetchCharacter() {
+        let url = "https://rickandmortyapi.com/api/character"
+        guard let url = URL(string: url) else { return }
+        
+        let session = URLSession(configuration: .default)
+        
+        session.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let info = try jsonDecoder.decode(Info.self, from: data)
+                print(info.results)
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+    }
 }
 
