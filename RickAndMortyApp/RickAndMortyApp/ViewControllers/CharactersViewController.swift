994bdc3006ba467fff2f2d56107eee892580c524
Orianka13 <odobrogor@gmail.com>
2023-01-09 17:50:57 +0400

    Передача данных на DetailVC

--- RickAndMortyApp/RickAndMortyApp/ViewControllers/CharactersViewController.swift
+++ RickAndMortyApp/RickAndMortyApp/ViewControllers/CharactersViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class CharactersViewController: UICollectionViewController {
     
     private var characters: [CharacterInfo] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchCharacter()
@@ -31,27 +31,34 @@ class CharactersViewController: UICollectionViewController {
             }
         }
     }
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+        if let detailsVC = segue.destination as? DetailsViewController {
+            guard let indexPath = collectionView.indexPathsForSelectedItems?.first else {
+                print("No indexPath")
+                return
+            }
+            detailsVC.character = characters[indexPath.item]
+        }
     }
-    */
-
+    
+    
     // MARK: UICollectionViewDataSource
-
-
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         characters.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterViewCell.reuseIdentifier, for: indexPath) as! CharacterViewCell
         
         let character = characters[indexPath.item]
+        
+        cell.layer.cornerRadius = 10
         
         NetworkManager.shared.fetchImage(from: character.image) { result in
             switch result {
@@ -61,44 +68,16 @@ class CharactersViewController: UICollectionViewController {
                 print(error)
             }
         }
-    
+        
         return cell
     }
     
-
     
-
+    
+    
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
-
+    
 }
 
 extension CharactersViewController: UICollectionViewDelegateFlowLayout {
